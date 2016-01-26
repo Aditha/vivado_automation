83504f8f5b5c7141e9df427ac016016286d6d6b0
Aditha Rajakaruna <aditha@paraqum.com>
2016-01-26 17:42:44 +0530

    improved the code to handle user options

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # vivado_automation
 automation scripts for xilinx vivado
+
+--------------------------------------------
+
+scripts are still under construction. thanks
--- inputs.tcl
+++ inputs.tcl
@@ -1,4 +1,5 @@
 # Inputs for Vivado Project
+set top_design      TOP_DESIGN
 set device          DEVICE
-set rtl_src_path    RTL_SRC_PATH
-set xdc_path        XDC_PATH
+set rtl_src_paths   RTL_SRC_PATH
+set xdc_paths       XDC_PATH
