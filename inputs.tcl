e875da4f26301128dde668c5001fda5a0b6a294a
Aditha Rajakaruna <aditha@paraqum.com>
2016-01-28 17:31:57 +0530

    updated the scripts

--- inputs.tcl
+++ inputs.tcl
@@ -1,5 +1,22 @@
+# Vivado run options
+set en_newrun 0
+set en_synth 0
+set en_place 0
+set en_route 0
+set en_bitgen 0
 # Inputs for Vivado Project
-set top_design      TOP_DESIGN
-set device          DEVICE
-set rtl_src_paths   RTL_SRC_PATH
-set xdc_paths       XDC_PATH
+set top_design              "TOP_DESIGN"
+set device                  "DEVICE"
+set rtl_src_paths           "RTL_SRC_PATH_1, RTL_SRC_PATH_2"
+set rtl_include_paths       "RTL_INCLUDE_PATH_1, RTL_INCLUDE_PATH_2"
+set xdc_paths               "XDC_PATH_1, XDC_PATH_2"
+# Synthesize Settings
+set s_constrset             ""
+set s_flattern_hierarchy    "none"
+set s_syn_directive         "default"
+# Placement Settings
+set s_place_directive       "default"
+# Route Settings
+set s_route_directive       "default"
+# Bit file Generation Engine
+# TODO
